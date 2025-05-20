f04c98b60ed684ff73d7610d5090925f377e2f0e
Dilara <dilaraozturk028@gmail.com>
2025-05-20 11:17:02 +0200

    chiffre

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,56 @@
 import { useState } from 'react'
 import './App.css'
-
+import Sidebar from './assets/img/sidebar.svg'
 function App() {
 
   return (
     <>
+    <section id='formulaire'>
+      <img id='sidebar' src={Sidebar} alt="" />
+      
+      <section className="overlay-text">
+      {/* point 1 */}
+        <div className='div-un'>
+          <div className='div-nbr'>
+            <p>1</p>
+          </div>
+          <div className='div-txt'>
+            <h5>STEP 1</h5>
+            <p>YOUR INFO</p>
+          </div>
+        </div>
+        {/* point 2 */}
+        <div className='div-1'>
+          <div className='div-nbr'>
+            <p>2</p>
+          </div>
+          <div className='div-txt'> 
+            <h5>STEP 2</h5>
+            <p>SELECT PLAN</p>
+          </div>
+        </div>
+        {/* point 3 */}
+        <div className='div-1'>
+          <div className='div-nbr'>
+            <p>3</p>
+          </div>
+          <div className='div-txt'>
+            <h5>STEP 3</h5>
+            <p>ADD-ONS</p>
+          </div>
+        </div>
+        {/* point 4 */}
+        <div className='div-1'>
+          <div className='div-nbr'>
+            <p>4</p>
+          </div>
+          <div className='div-txt'>
+            <h5>STEP 4</h5>
+            <p>SUMMARY</p>
+          </div>
+        </div>
+      </section>
+    </section>
     </>
   )
 }
